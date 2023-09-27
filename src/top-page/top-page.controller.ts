1a3c594c3a740e61141aad05912d7645bbe90b25
unknown <bogdanstarikov011@gmail.com>
2023-09-27 22:11:18 +0300

    Работа с переменными окружения

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService) {}
+	@Get(':id')
+	async get(@Param('id') id: string) {
+		this.configService.get('Test');
+	}
 	@Post('create')
 	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
-	@Get(':id')
-	async get(@Param('id') id: string) {}
 	@Delete(':id')
 	async delete(@Param('id') id: string) {}
 	@Patch(':id')
