19de08a40a62e7eac34cca932b3172225af9b737
unknown <bogdanstarikov011@gmail.com>
2023-10-09 21:15:40 +0300

    Connecting to MongoDB using Mongoose

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
-	constructor(private readonly configService: ConfigService) {}
-	@Get(':id')
-	async get(@Param('id') id: string) {
-		this.configService.get('Test');
-	}
 	@Post('create')
 	async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
+
+	@Get(':id')
+	async get(@Param('id') id: string) {}
+
 	@Delete(':id')
 	async delete(@Param('id') id: string) {}
+
 	@Patch(':id')
 	async patch(@Param('id') id: string, @Body() dto: TopPageModel) {}
+
 	@HttpCode(200)
 	@Post()
 	async find(@Body() dto: FindTopPageDto) {}
